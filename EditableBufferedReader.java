0df201bc4c54406bc59be67edf88e78428ac47fc
Xandri-Roca <71765907+Xandri-Roca@users.noreply.github.com>
2020-10-06 15:25:40 +0200

    Create EditableBufferedReader.java

---
+++ EditableBufferedReader.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.lang.Runtime;
+import java.lang.Process;
+public class EditableBufferedReader extends BufferedReader {
+
+
+    public EditableBufferedReader(Reader r){
+        super(r);
+    }
+
+    // switch from cooked mode to raw mode in the console
+    public void setRaw(){
+        try{
+            String[] cmd = {"/bin/sh", "-c", "stty -echo raw </dev/tty"};
+            Process newProcess1=Runtime.getRuntime().exec(cmd);
+            newProcess1.waitFor();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void unsetRaw(){
+        try{
+
+        	String[] cmd = {"/bin/sh", "-c", "stty echo cooked </dev/tty"}
+            Process newProcess=Runtime.getRuntime().exec(cmd);
+            newProcess.waitFor();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
