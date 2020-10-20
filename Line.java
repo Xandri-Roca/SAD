1ff50282c48727287050d531e0b21d3a8a29faf5
Xandri-Roca <71765907+Xandri-Roca@users.noreply.github.com>
2020-10-20 23:06:42 +0200

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,106 @@
+import java.io.*;
+
+public class Line{
+	// maintains the status of the line in edition with its corresponding methods.
+
+	private char[] phrase;
+	private int cursorPosition;
+	private int maxSize = 500;  // random value??
+	private int numLetters;
+	//private int insertar;
+	private boolean insertar;
+
+
+	public Line(){
+		phrase = new char[maxSize];
+		cursorPosition = 0;
+		numLetters = 0;
+		//insertar = 0;
+		insertar = false;
+
+	}
+
+	public void moveRight(){
+		cursorPosition ++;
+	}
+
+	public void moveLeft(){
+		cursorPosition --;
+	}
+
+	public void moveToInicio(){
+		cursorPosition = 0;
+	}
+
+	public void moveToFin(){
+		cursorPosition = numLetters;
+	}
+
+	public void insertar(){
+		//insertar ++;
+		//insertar ^= insertar;
+		if(insertar == true){
+			insertar = false;
+		}else{
+			insertar = true;
+		}
+		System.out.println(insertar);
+	}
+
+	public void suprimir(){
+		for(int i = cursorPosition; i < numLetters; i++){
+			phrase[i] = phrase[i + 1];
+		}
+
+		numLetters --;
+	}
+
+	public void retroceso(){
+		for(int i = cursorPosition; i < numLetters; i++){
+			phrase[i - 1] = phrase[i]; 
+		}
+
+		cursorPosition --;
+		numLetters --;
+	}
+
+	public void write(int letterId) {
+		// special situation when the suprimir is activated.
+		char letter = (char) letterId; // The char convertion follows the int Unicode - ASCII table (not like python)
+		if(insertar == false){
+			// if the cursor is not in the last position:
+			if (cursorPosition != numLetters){
+				for(int i = numLetters; i > cursorPosition; i--){
+					phrase[i]=phrase[i-1];
+				}
+			}
+			phrase[cursorPosition] = letter;
+			numLetters ++;
+			cursorPosition ++;
+		}
+		
+
+		if(insertar == true){
+			phrase[cursorPosition] = letter;
+			numLetters ++;
+			cursorPosition ++;
+		}
+	}
+
+	public String getPhrase(){
+		return String.valueOf(phrase);
+	}
+
+	public int getCursorPosition(){
+		return cursorPosition;
+	}
+
+	public int getNumLetters(){
+		return numLetters;
+	}
+
+	public boolean getInsertarState(){
+		return insertar;
+	}
+
+}
